ce36390e90b54a15030d2d38fd860266384178f9
Beerus-Lee <408964891@qq.com>
2021-08-21 09:58:40 +0800

    push code

---
+++ arithmetic/src/main/java/leetcode/InitListNode.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class InitListNode {
+
+    static int[] arr = {};
+
+    public static ListNode init(int[] array){
+        arr = array;
+        return putValue(0);
+    }
+
+    public static ListNode putValue(int i){
+        if (i >= arr.length) {
+            return null;
+        }
+        ListNode node = new ListNode(arr[i],putValue(++i));
+
+         return node;
+    }
+}
